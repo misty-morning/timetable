7b3e819ffb20cdeb180ddaf3ffae3393cff06dc9
andrey_kharchenko <monk_of_emptiness@mail.ru>
2016-02-25 19:05:51 +0300

    ui

---
+++ index.js
@@ -0,0 +1,25 @@
+var ui = {
+	table: "#table",
+	selectTable: "#select-table",
+	newTable: "#new-table",
+	deleteTable: "#delete-table",
+	tableNameInput: "#table-name-input",
+	tableNameOk: "#new-table-ok",
+	newStation: "#new-station",
+	deleteStation: "#delete-station",
+	stationNameInput: "#station-name-input",
+	stationTimeInput: "#station-time-input",
+	newStationOk: "#new-station-ok",
+	startTable: "#start-table",
+	stopTable: "#stop-table",
+};
+window.App = {
+    Models: {},
+    Collections: {},
+    Views:{}
+};
+$(document).ready(function() {
+
+
+});
+
